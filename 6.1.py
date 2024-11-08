649b6593447d67e1b2010e6e007de580415baf7e
AndrewHrabrov <medveddimon623@gmail.com>
2024-11-08 14:18:02 +0300

    task 1

---
+++ 6.1.py
@@ -0,0 +1,41 @@
+import argparse
+import secrets
+
+class Password:
+    def __init__(self, set_symbols):
+        self.set_symbols = set_symbols
+        self.required_symbols = [secrets.choice(symbol) for symbol in self.set_symbols]
+
+    def generate_password(self, length):
+        chars = "".join(self.set_symbols)
+        password = "".join(secrets.choice(chars)
+                        for _ in range(length - len(self.required_symbols)))
+        password = "".join(secrets.choice(password + "".join(self.required_symbols))
+                        for _ in range(len(password + "".join(self.required_symbols))))
+        return password
+
+parser = argparse.ArgumentParser(
+    prog = 'Generator of passwords',
+    description = 'Program generate passwords',
+    epilog = 'Enter a length of passwords, set of symbols n number of generate passwords'
+)
+
+parser.add_argument('-l', '--len',
+                    type=int,
+                    required=True,
+                    help='length of passwords')
+
+parser.add_argument('-c', '--count',
+                    type=int,
+                    required=True,
+                    help='Count of passwords')
+
+parser.add_argument('-s', '--set',
+                    type=str,
+                    required=True,
+                    help='Set of symbols, that using in generate passwords')
+
+args = parser.parse_args()
+password_generator = Password(args.set)
+for i in range(args.count):
+    print(password_generator.generate_password(length=args.len))
